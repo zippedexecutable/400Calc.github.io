baf4914a0125198144295e2adfad17c41c4572f3
zippedexecutable <executable22@gmail.com>
2025-01-15 23:04:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 
 app.use(cors());
 app.use(bodyParser.json());
